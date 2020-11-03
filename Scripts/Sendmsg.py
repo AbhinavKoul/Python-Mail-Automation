b11e50d5d580d456ee54ceb99b908c93b8b672c7
AbhinavKoul <47354541+AbhinavKoul@users.noreply.github.com>
2020-11-04 00:33:41 +0530

    gfdgs

--- Scripts/Sendmsg.py
+++ Scripts/Sendmsg.py
@@ -8,17 +8,69 @@ import os
 
 html = """\
 <html>
-  <body>
-    <br> <img src="cid:image1" width="500" height="600"> <br>
-    <p>Hi,{{ name }}<br>
-       How are you?<br>
-       <a href="https://www.teamstardust.org/">Join Team Stardust</a> 
-       for exiting times
-    </p>
-  </body>
+	<head> 
+		<title> Orientation </title>
+	</head>
+	<body style="background-color:#ede8e8;padding:10px;font-family:'Arial'">
+		<div style="padding:50px;">
+			<p> Hi {{ name }}, </p> <br>
+
+			<p>
+				Thank you for booking your place for the orientation session of the only space team of RIT- STARDUST!
+				We can’t wait to greet you and show you all the amazing things we have been up to. <br><br> 
+				The session starts at <b style="color:red;"><i>7 pm</b></i> on <b style="color:red;"><i>November the 6th 2020</b></i>. 
+				You can find about our team and the various subsystems in the brochure attached below:
+			</p>
+			<br>
+			<img src="cid:image1" alt="brochure" style="margin-left:auto;
+			margin-right:auto;
+			display:block;
+			height:600px;
+			width:450px;"> 
+		<br>
+		<br>
+		<div style="text-align:center;">
+		<a href={{ meet_link }} class="button" style="background-color: #9966ff;
+			  border: none;
+			  color: white;
+			  padding: 15px 32px;
+			  text-decoration:none;
+			  text-align: center;
+			  display: inline-block;
+			  font-size: 25px;
+			  margin: 4px 2px;
+			  border-radius:12px;
+			cursor: pointer;"> Click to join the meeting </a><br>
+		<p><i> Or alternatively, use this link to join: <a href = {{ meet_link }}> Join Meeting </i> </a> 
+		
+		</div>
+		<br>
+		Please try to make it on time so that you can witness the entire session. 
+
+		If you have any clarification you can get in touch with us via this email and we will try to clarify it as soon as possible. <br> <br><br>		
+
+		Hope to see you soon, <br><br>
+
+		All the best,
+		The Stardust Recruiting Team
+
+		<br>
+		<br>
+		<img src="cid:logo" style="height:90px;width:90px;">
+
+		<br>
+		Want to sneak a backstage peek? Follow Life at Stardust
+		on <a href=https://www.linkedin.com/company/rit-stardust><i>LinkedIn</i></a>, <a href="https://www.instagram.com/rit_stardust"><i>Instagram</i></a>, and <a href ="https://www.youtube.com/channel/UC61xGOFapcIeAWdQvtCffJw"> <i>Youtube</i></a>
+
+		</div>
+	</body>
 </html>
+	
+
 """
 
+
+meeting_link = "https://meet.google.com/uja-ofrp-chj"
 
 root = "./Data/Test/"
 data = pd.read_csv(os.path.join(root,"Testing.csv"))
@@ -27,22 +79,15 @@ data["Mail Status"] = "NULL"
 total = data["Email Address"].count()
 
 
-sender_email = "stardust.recruitment@gmail.com"
-# receiver_email = "abhinav2scientist@gmail.com"
-password = "Automation@1234"
+sender_email = "stardust.recr@gmail.com"
+password = "sdrecr2020"
 
 message = MIMEMultipart("alternative")
 message["Subject"] = "Orientation Invite TEST - SCRIPT"
 message["From"] = sender_email
-# message["To"] = receiver_email
-# nam = "Abhinav Koul"
 
 template = Template(html)
 
-# # Create the plain-text and HTML version of your message
-# text = """\
-# Hi,
-# How are you?"""
 
 # This example assumes the image is in the current directory
 img_path = "./Images/img.jpg"
@@ -52,6 +97,16 @@ fp.close()
 
 # Define the image's ID as referenced above
 msgImage.add_header('Content-ID', '<image1>')
+message.attach(msgImage)
+
+# This example assumes the image is in the current directory
+img_path = "./Images/stardust-logo.png"
+fp = open(img_path, 'rb')
+msgImage = MIMEImage(fp.read())
+fp.close()
+
+# Define the image's ID as referenced above
+msgImage.add_header('Content-ID', '<logo>')
 message.attach(msgImage)
 
 
@@ -74,7 +129,7 @@ with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
 
             # Turn these into plain/html MIMEText objects
             # part1 = MIMEText(text, "plain")
-            part2 = MIMEText(template.render(name = Name), "html")
+            part2 = MIMEText(template.render(name = Name,meet_link = meeting_link), "html")
 
                     
             # Add HTML/plain-text parts to MIMEMultipart message
